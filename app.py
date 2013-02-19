9d5dc7c56d344feec67505a09f565beaac9e39e0
Aaron Torres <torresaa@arctic.cse.msu.edu>
2013-02-19 15:41:53 -0500

    Finished day13!

--- app.py
+++ app.py
@@ -11,11 +11,27 @@ class SimpleApp(object):
             content_type = 'text/html'
             data = """\
 Visit:
+<a href='linkz'>cse 491 linkz stuff</a>
 <a href='content'>a file</a>,
 <a href='error'>an error</a>,
 <a href='helmet'>an image</a>,
 <a href='somethingelse'>something else</a>
 """
+        elif path == '/linkz':
+            content_type = 'text/html'
+            data = open('html/index.html').read()
+        elif path == '/link.html':
+            content_type = 'text/html'
+            data = open('html/link.html').read()
+        elif path == '/subdir/table.html':
+            content_type = 'text/html'
+            data = open('html/subdir/table.html').read()
+        elif path == '/catastrophe.html':
+            content_type = 'text/html'
+            data = open('html/catastrophe.html').read()
+        elif path == '/index.html':
+            content_type = 'text/html'
+            data = open('html/index.html').read()
         elif path == '/content':
             content_type = 'text/html'
             data = open('somefile.html').read()
